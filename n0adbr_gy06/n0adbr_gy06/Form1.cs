a76267ffd38c9563d09cd0b914e111bb9fa455c5
Szabolcs <szabolcs.pinter@stud.uni-corvinus.hu>
2021-10-29 17:39:09 +0200

    gyak06

--- n0adbr_gy06/n0adbr_gy06/Form1.cs
+++ n0adbr_gy06/n0adbr_gy06/Form1.cs
@@ -12,6 +12,17 @@ using System.Windows.Forms;
 
 namespace n0adbr_gy06
 {
+    var mnbService = new MNBArfolyamServiceSoapClient();
+
+    var request = new GetExchangeRatesRequestBody()
+    {
+        currencyNames = "EUR",
+        startDate = "2020-01-01",
+        endDate = "2020-06-30"
+    };
+    var response = mnbService.GetExchangeRates(request);
+    var result = response.GetExchangeRatesResult;
+
     public partial class Form1 : Form
     {
        
