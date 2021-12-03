8070e7e49f688f56c23bb617d036c26997951c63
Szabolcs <szabolcs.pinter@stud.uni-corvinus.hu>
2021-12-03 19:25:45 +0100

    gyak9_2

---
+++ gyak9_n0adbr/gyak9_n0adbr/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak9_n0adbr.Entities
+{
+    class DeathProbability
+    {
+        public bool Gender { get; set; }
+        public int Age { get; set; }
+        public double DeathProb { get; set; }
+    }
+}
